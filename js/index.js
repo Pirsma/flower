5fa7a9e1a2f78d08b4c4a826a8fdc0839b9e557e
Pirsma <simcoc118@gmail.com>
2026-03-31 04:05:34 +0900

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = 'Amii, aku punya sesuatu buat kamu'.split('')
+const text = 'Amii, i have something for u'.split('')
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
     title.innerHTML += `<span>${text[index]}<span/>`
